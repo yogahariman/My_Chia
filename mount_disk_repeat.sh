9673ee9f8a0bcda4d49c9a0fb587412ae5a1181c
Yoga Hariman <yogahariman.course@gmail.com>
2021-08-16 21:37:16 +0700

    Update mount_disk_repeat.sh

--- mount_disk_repeat.sh
+++ mount_disk_repeat.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
 #sudo visudo; rfi212 ALL=(ALL) NOPASSWD: /bin/mount
 st=true
 while $st
 do
-	for i in {1..3}
+	for i in {1..4}
 	do
-		if [[ $i -lt 10 ]]; then drive="/Drive/00$i"; fi
-		if [[ $i -gt 9 ]]; then drive="/Drive/0$i"; fi
-		time_now=$(date '+%Y/%m/%d %H:%M:%S')
-		[ ! -d $drive ] && mount -a && echo "$time_now Directory $drive DOES NOT exists." >> /home/rfi212/Documents/mount.log
-		#[ ! -d $drive ] && echo "Directory $drive DOES NOT exists."
-		#if [[ -d $drive ]]; then
-		#	echo "$drive exists."
-		#fi
+		if [[ $i -lt 10 ]]; then drive="/Drive/00$i/Plots/"; fi
+		if [[ $i -gt 9 ]]; then drive="/Drive/0$i/Plots/"; fi
+		#[[ ! -d $drive ]]&echo "Directory $drive DOES NOT exists."
+		if [[ ! -d $drive ]]; then
+			time_now=$(date '+%Y/%m/%d %H:%M:%S')
+			mount -a
+			echo "Directory $drive DOES NOT exists."
+			echo "$time_now Directory $drive DOES NOT exists." >> /home/rfi212/mount.log
+		fi
 	done
 	#st=false
 	sleep 300
