7c41c431b7350c9bce36a199df24709221721a57
Yoga Hariman <yogahariman.course@gmail.com>
2021-08-16 21:36:46 +0700

    Update farming_chia_and_forks.sh

--- farming_chia_and_forks.sh
+++ farming_chia_and_forks.sh
@@ -13,22 +13,3 @@ source /home/rfi212/chia-blockchain/venv/bin/activate; chia start farmer;deactiv
 #flax show -a node-2.flaxnetwork.org:6888
 #flax show -a node-3.flaxnetwork.org:6888
 #deactivate
-
-#sudo visudo; rfi212 ALL=(ALL) NOPASSWD: /bin/mount
-st=true
-while $st
-do
-	for i in {1..3}
-	do
-		if [[ $i -lt 10 ]]; then drive="/Drive/00$i"; fi
-		if [[ $i -gt 9 ]]; then drive="/Drive/0$i"; fi
-		time_now=$(date '+%Y/%m/%d %H:%M:%S')
-		[ ! -d $drive ] && mount -a && echo "$time_now Directory $drive DOES NOT exists." >> /home/rfi212/Documents/mount.log
-		#[ ! -d $drive ] && echo "Directory $drive DOES NOT exists."
-		#if [[ -d $drive ]]; then
-		#	echo "$drive exists."
-		#fi
-	done
-	#st=false
-	sleep 300
-done
