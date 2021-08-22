bc9ed8eb83269a2830f117f367f8f076827286bb
Yoga Hariman <yogahariman.course@gmail.com>
2021-08-22 10:34:52 +0700

    Update farming_chia_and_forks.sh

--- farming_chia_and_forks.sh
+++ farming_chia_and_forks.sh
@@ -1,15 +1,21 @@
 #! /bin/sh
 # chia
-#source ~/chia-blockchain/activate
-source /home/rfi212/chia-blockchain/venv/bin/activate; chia start farmer;deactivate;
-#sleep 300
+source ~/chia-blockchain/venv/bin/activate; chia start farmer;deactivate;
+#sleep 600
+# flax
+source ~/flax-blockchain/venv/bin/activate; flax start farmer; deactivate;
+#sleep 600
+# flax
+source ~/silicoin-blockchain/venv/bin/activate; silicoin start farmer; deactivate;
+#sleep 600
+
+
+#source /home/rfi212/chia-blockchain/venv/bin/activate
 #chia show -a node.chia.net:8444
 #deactivate
-# flax
-#source ~/flax-blockchain/activate
-#flax start farmer
-#sleep 300
-#flax show -a node-1.flaxnetwork.org:6888
-#flax show -a node-2.flaxnetwork.org:6888
-#flax show -a node-3.flaxnetwork.org:6888
-#deactivate
+
+source ~/flax-blockchain/venv/bin/activate
+flax show -a node-1.flaxnetwork.org:6888
+flax show -a node-2.flaxnetwork.org:6888
+flax show -a node-3.flaxnetwork.org:6888
+deactivate
